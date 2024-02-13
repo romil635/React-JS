f35c3c14f3804d7b4e1b064c6913b7d12896726f
romil635 <139975938+romil635@users.noreply.github.com>
2024-02-13 17:19:22 +0530

    13-2-24

---
+++ javascript/TypeCoversion/Tc.js
@@ -0,0 +1,73 @@
+/* Javascript type Conversion */
+
+// let x;
+// console.log(x); /*"undefined" */
+// console.log(typeof(x));
+
+
+// const x; /* SyntaxError: Missing initializer in const declaration */
+
+
+// console.log(x); /* ReferenceError */
+// const x = 3;
+
+// console.log(y); /* ReferenceError */
+// let y = 3;
+
+
+// x = "The answer is " + 42;   /* "The answer is 42" */
+// y = 42 + " is the answer";   /* "42 is the answer" */
+// z = "37" + 7;                /* "377" */
+
+// console.log(x);
+// console.log(typeof(x));
+// console.log(y);
+// console.log(typeof(y));
+// console.log(z);
+// console.log(typeof(z));
+
+{
+//   let x = "37" - 7;
+//   let y = "7" * 7; 
+  // console.log(x);
+  // console.log(typeof(x));
+  // console.log(y);
+  // console.log(typeof(y));
+}
+
+/* convert string to number */
+
+{
+  // let x = false;
+  // let x = true;
+  // let x = 0;
+  // let x = 1;
+  // let x = "000";
+  // let x = "111"
+  // let x = undefined;
+  // let x = NaN;
+  // let x = null;
+  // let x = Infinity;
+  // let x = -Infinity;
+  // let x = " ";
+  // let x = [ ];
+  // let x = ["Hello"];
+  // let x = [10, 50 ,30];
+  // let x = [100];
+  // let x = ["Hello" , "Hi"];
+  // let x =function(){};
+  // let x = {};
+//   console.log(typeof(x));
+  
+//   let y = (Number(x));
+//   console.log(y);
+//   console.log(typeof(y));
+  
+//   let z = (String(x));
+//   console.log(z);
+//   console.log(typeof(z));
+
+//   let a = (Boolean(x));
+//   console.log(a);
+//   console.log(typeof(a));
+}
